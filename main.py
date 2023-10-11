73c84059cf0244f45b37b3323406e447cf2b64b2
lukasjoho <social@lukashoppe.com>
2023-10-11 22:43:09 +0200

    build: fix https db connection

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
+import clickhouse_connect
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = os.environ.get('HOST')
+PORT = os.environ.get('PORT')
+USERNAME = os.environ.get('USERNAME')
+PASSWORD = os.environ.get('PASSWORD')
+INTERFACE = os.environ.get('INTERFACE')
+
+client = clickhouse_connect.get_client(interface=INTERFACE, host=HOST, port=PORT, username=USERNAME, password=PASSWORD, verify=True, ca_cert="isrgrootx1.pem")
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return "Analytics API running"
+    return "Application running"
+
+@app.get("/track")
+def read_track():
+    query_result = client.query('SELECT * FROM events')
+    rows = query_result.result_rows
+    columns = query_result.column_names
+    json = [dict(zip(columns, row)) for row in rows]
+    return json
 
 @app.post("/track")
 def track_event():
@@ -14,3 +35,4 @@ def track_event():
 @app.post("/page")
 def page_event():
     return "Page tracked"
+
